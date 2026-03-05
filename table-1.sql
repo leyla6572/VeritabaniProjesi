ac7cc751d3e0d86e25a0ebf3727fddd5f3ea702c
Leyla Berhuş <leylaberhus@gmail.com>
2026-03-05 18:27:34 +0300

    yass

---
+++ table-1.sql
@@ -0,0 +1,14 @@
+﻿USE [Çevrimiçi Yemek Sipariş Platformu]
+GO
+
+/****** Object:  Table [dbo].[Table_1]    Script Date: 5.03.2026 18:23:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Table_1](
+	[yas] [nchar](10) NULL
+) ON [PRIMARY]
+GO
